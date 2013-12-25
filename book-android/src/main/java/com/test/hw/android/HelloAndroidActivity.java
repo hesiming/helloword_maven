ef54d0e8112a159658bc426015809393f2d27280
hesiming <hesiming1@126.com>
2013-12-25 18:20:48 +0800

    提交子项目的代码

---
+++ book-android/src/main/java/com/test/hw/android/HelloAndroidActivity.java
@@ -0,0 +1,14 @@
+package com.test.hw.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloAndroidActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
